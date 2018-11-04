9337953800fd9c541fd62637e6951f9d724f59e3
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2018-11-03 20:44:56 -0700

    Working sudoku challenge - needs testing

--- src/App.js
+++ src/App.js
@@ -1,59 +1,8 @@
 import React, { Component } from "react";
-import generator from "sudoku";
+import { generateSudoku, checkSolution, shareUrl } from "./lib/sudoku";
 import produce from "immer";
 import SudokuBoard from "./components/SudokuBoard";
 import "./App.css";
-
-/*
-  Generates a sudoku with the structure
-
-  {rows: [{index: 0, cols: [{row: 0, col: 0, value: 1, readonly: true}, ...]}, ...]}
-
-*/
-function generateSudoku() {
-  const raw = generator.makepuzzle();
-  const rawSolution = generator.solvepuzzle(raw);
-
-  const formatted = raw.map(e => (e === null ? null : e + 1));
-  const formattedSolution = rawSolution.map(e => e + 1);
-
-  const result = {
-    rows: [],
-    solution: formattedSolution,
-    startTime: new Date(),
-    solvedTime: null
-  };
-
-  for (let i = 0; i < 9; i++) {
-    const row = { cols: [], index: i };
-    for (let j = 0; j < 9; j++) {
-      const value = raw[i * 9 + j];
-      const col = {
-        row: i,
-        col: j,
-        value: value,
-        readonly: value !== null
-      };
-      row.cols.push(col);
-    }
-    result.rows.push(row);
-  }
-
-  return result;
-}
-
-function checkSolution(sudoku) {
-  const candidate = sudoku.rows
-    .map(row => row.cols.map(col => col.value))
-    .flat();
-
-  for (let i = 0; i < candidate.length; i++) {
-    if (candidate[i] === null || candidate[i] !== sudoku.solution[i]) {
-      return false;
-    }
-  }
-  return true;
-}
 
 class App extends Component {
   constructor(props) {
@@ -71,6 +20,7 @@ class App extends Component {
           const solved = checkSolution(state.sudoku);
           if (solved) {
             state.sudoku.solveTime = new Date();
+            state.sudoku.shareUrl = shareUrl(state.sudoku);
           }
         }
       })
@@ -96,8 +46,6 @@ class App extends Component {
           <h1>Sudoku Stack</h1>
         </header>
         <SudokuBoard sudoku={this.state.sudoku} onChange={this.handleChange} />
-
-        <button onClick={this.solveSudoku}>Solve it Magically!</button>
       </div>
     );
   }
---
+++ src/lib/sudoku.js
@@ -0,0 +1,77 @@
+import generator from "sudoku";
+
+/*
+  Generates a sudoku with the structure
+
+  {rows: [{index: 0, cols: [{row: 0, col: 0, value: 1, readonly: true}, ...]}, ...]}
+
+*/
+export function generateSudoku() {
+  const fromUrl = extractUrlData();
+  console.log("Got from url: ", fromUrl);
+  const raw = fromUrl ? fromUrl.raw : generator.makepuzzle();
+  const rawSolution = generator.solvepuzzle(raw);
+
+  const formatted = raw.map(e => (e === null ? null : e + 1));
+  const formattedSolution = rawSolution.map(e => e + 1);
+
+  const result = {
+    raw,
+    rows: [],
+    solution: formattedSolution,
+    startTime: new Date(),
+    solvedTime: null,
+    challengerStartTime: fromUrl && fromUrl.startTime,
+    challengerSolvedTime: fromUrl && fromUrl.solvedTime
+  };
+
+  for (let i = 0; i < 9; i++) {
+    const row = { cols: [], index: i };
+    for (let j = 0; j < 9; j++) {
+      const value = raw[i * 9 + j];
+      const col = {
+        row: i,
+        col: j,
+        value: value,
+        readonly: value !== null
+      };
+      row.cols.push(col);
+    }
+    result.rows.push(row);
+  }
+
+  return result;
+}
+
+export function checkSolution(sudoku) {
+  const candidate = sudoku.rows
+    .map(row => row.cols.map(col => col.value))
+    .flat();
+
+  for (let i = 0; i < candidate.length; i++) {
+    if (candidate[i] === null || candidate[i] !== sudoku.solution[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function shareUrl(sudoku) {
+  const data = {
+    raw: sudoku.raw,
+    startTime: sudoku.startTime,
+    solvedTime: sudoku.solvedTime
+  };
+  const query = btoa(JSON.stringify(data));
+
+  return document.location.href.replace(/\?.+$/, "") + `?sudoku=${query}`;
+}
+
+function extractUrlData() {
+  const match = document.location.search.match(/sudoku=([^&]+)/);
+
+  if (match) {
+    return JSON.parse(atob(match[1]));
+  }
+  return null;
+}
